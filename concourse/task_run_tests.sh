f0f19cf83b5a155555c82a8ea9913a76a4cf2c35
Beth Long <bethlong@newrelic.com>
2020-03-12 05:37:08 -0700

    Add concourse task

---
+++ concourse/task_run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+# $GOPATH is gopath/ folder
+export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app/Godeps/_workspace
+cd gopath/src/github.com/bethadele/simple-go-web-app/
+
+echo "About to run tests..."
+go test ./...
+
