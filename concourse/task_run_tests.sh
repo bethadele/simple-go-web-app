d7864eab0d119dde67e16a41ba74526dd1835fca
Beth Long <bethlong@newrelic.com>
2020-03-12 05:51:15 -0700

    Repair path for task

--- concourse/task_run_tests.sh
+++ concourse/task_run_tests.sh
@@ -3,7 +3,7 @@
 set -e
 
 # $GOPATH is gopath/ folder
-export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app/Godeps/_workspace
+export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/bethadele/simple-go-web-app/Godeps/_workspace
 cd gopath/src/github.com/bethadele/simple-go-web-app/
 
 echo "About to run tests..."
