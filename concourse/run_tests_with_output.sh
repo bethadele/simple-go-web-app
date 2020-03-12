9c6e144dd41d77a8294b3685cdfe9bea55d9945a
Beth Long <bethlong@newrelic.com>
2020-03-12 11:44:34 -0700

    Add parallel test task, script

---
+++ concourse/run_tests_with_output.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+# $GOPATH is gopath/ folder
+export MAINPATH=$(pwd)
+export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/bethadele/simple-go-web-app/Godeps/_workspace
+cd gopath/src/github.com/bethadele/simple-go-web-app/
+
+pwd > test-results.txt
+echo "nTest results:\n" >> test-results.txt
+go test ./... >> test-results.txt
+mv test-results.txt $MAINPATH/app-test-results/
